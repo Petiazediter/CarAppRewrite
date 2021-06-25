66db6b85fb1ada312289bc86d05b19abb80b9389
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-25 18:37:50 +0200

    Add design to wrapper layout with styled component.

--- car-app-frontend/src/App.tsx
+++ car-app-frontend/src/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; 
 import { About } from "./components/about-page/About";
 import { Layout } from 'antd'
+import styled from '@emotion/styled';
+
+
+const ContentLayout = styled(Layout)`
+  width:100%;
+  @media only screen and (min-width:800px){
+    width:750px;
+    margin:auto;
+  }
+`;
 
 function App() {
   return (
-    <Layout>
+    <ContentLayout>
       <BrowserRouter>
         <Route path="/" exact>
           Home page
@@ -14,7 +24,7 @@ function App() {
           <About/>
         </Route>
       </BrowserRouter>
-    </Layout>
+    </ContentLayout>
   );
 }
 
