964e5dca505fc2b48e8384c5f4a8d2b1fdf0bbe0
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-25 16:18:26 +0200

    Create styled component from layout.

--- car-app-frontend/src/components/about-page/About.tsx
+++ car-app-frontend/src/components/about-page/About.tsx
@@ -1,14 +1,25 @@
 import styled from "@emotion/styled"
+import { Layout } from 'antd'
 
 const AboutHeader = styled('h1')({
     width: 'fit-content',
     fontFamily: 'sans-serif',
     borderBottom: '10px solid #676CE1',
+    fontSize: 'xx-large',
+    color: '#353535'
+})
+
+const Content = styled(Layout)({
+    padding: '2em',
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    position: 'relative',
+    width: '100%'
 })
 
 export function About() {
     return (
-    <div>
-        <AboutHeader>Hello world</AboutHeader>
-    </div>)
+    <Content>
+        <AboutHeader>Let us introduce ourself</AboutHeader>
+    </Content>)
 }
