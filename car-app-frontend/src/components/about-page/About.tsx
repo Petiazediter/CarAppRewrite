16b8de5a357ea1b9bbd99d72cc23c9b9b8c5d957
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-25 16:44:26 +0200

    Create new custom component for describe the page in segments.

--- car-app-frontend/src/components/about-page/About.tsx
+++ car-app-frontend/src/components/about-page/About.tsx
@@ -17,9 +17,23 @@ const Content = styled(Layout)({
     width: '100%'
 })
 
+const DescriptionWithIconStyled = styled('div')({
+
+})
+
 export function About() {
     return (
     <Content>
         <AboutHeader>Let us introduce ourself</AboutHeader>
+        <DescriptionWithIcon title="Test" description="Ex adipisicing laborum est velit id do deserunt. Aute officia laboris qui mollit proident. Consectetur do excepteur ex ullamco et dolore dolore sit ullamco do. Qui officia excepteur aliquip reprehenderit officia excepteur. Est ipsum quis voluptate Lorem labore nostrud."/>
     </Content>)
+}
+
+function DescriptionWithIcon({title, description} : { title: string, description: string}) {
+    return ( 
+        <DescriptionWithIconStyled>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </DescriptionWithIconStyled>
+    )
 }
