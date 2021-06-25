2d0d1ebfebba7753ebe84473ac97491546ed242c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-25 18:35:10 +0200

    Wrap items to layout in App.tsx

--- car-app-frontend/src/App.tsx
+++ car-app-frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; 
 import { About } from "./components/about-page/About";
+import { Layout } from 'antd'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/" exact>
-        Home page
-      </Route>
-      <Route path="/about">
-        <About/>
-      </Route>
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <Route path="/" exact>
+          Home page
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+      </BrowserRouter>
+    </Layout>
   );
 }
 
