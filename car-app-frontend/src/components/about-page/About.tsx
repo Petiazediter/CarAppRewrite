1f2f782d5b9a20b5f2d9e815a834d2fd16f3ca13
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-25 16:10:46 +0200

    Create styled component.

--- car-app-frontend/src/components/about-page/About.tsx
+++ car-app-frontend/src/components/about-page/About.tsx
@@ -1,9 +1,14 @@
 import styled from "@emotion/styled"
 
+const AboutHeader = styled('h1')({
+    width: 'fit-content',
+    fontFamily: 'sans-serif',
+    borderBottom: '10px solid #676CE1',
+})
 
 export function About() {
     return (
     <div>
-        <h1>Hello world</h1>
+        <AboutHeader>Hello world</AboutHeader>
     </div>)
 }
