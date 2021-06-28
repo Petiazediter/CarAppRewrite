25df27a11f256257e70b6b7b070449c854aeae63
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-28 08:39:08 +0200

    Replace FilledInput with AntDesign's search bar.

--- car-app-frontend/src/components/navbar-component/Navbar.tsx
+++ car-app-frontend/src/components/navbar-component/Navbar.tsx
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled"
-import { Row, Col, Menu, Dropdown } from "antd"
+import { Row, Col, Menu, Dropdown, Input } from "antd"
 import { Link } from 'react-router-dom'
 import useWindowDimensions from '../../hooks/WindowSize'
 import { MenuOutlined } from '@ant-design/icons'
+
+const { Search } = Input;
 
 interface NavbarMenuColProps{
     isHighlighted?: boolean;
@@ -26,17 +28,6 @@ const StyledNav = styled('nav')`
     margin-bottom: 1em;
     box-shadow: 0px 1px 0px rgba(100,100,100,.2);
     padding-bottom: 5px;
-`
-
-const FilledRowInput = styled('input')`
-    position:relative;
-    width:100%;
-    height:100%;
-    outline:none;
-    border:none;
-    font-weight:bold;
-    background:#FCFCFC;
-    padding:1em;
 `
 
 const NavbarLink = styled(Link)<NavbarMenuColProps>`
@@ -81,6 +72,16 @@ const menu = (
   </Menu>
 );
 
+const onSearch = (term: string) => {
+    console.log(term)
+}
+
+const searchStyle ={
+    width:'100%',
+    height:'100%',
+    padding:'5px'
+}
+
 export function Navbar() {
     const { width } = useWindowDimensions();
 
@@ -101,7 +102,7 @@ export function Navbar() {
                 <NavbarLink to="/about">About us</NavbarLink>
             </NavbarMenuCol>
             <NavbarMenuCol flex={4}>
-                <FilledRowInput type="text" placeholder="search"/>
+            <Search placeholder="Search for cars" onSearch={onSearch} style={searchStyle} />
             </NavbarMenuCol>
             <NavbarMenuCol isHighlighted flex={1}>
                 <NavbarLink isHighlighted to="/sign-in">Sign in</NavbarLink>
@@ -114,7 +115,7 @@ export function Navbar() {
                 <TitleLink to="/"><LeftAlignedH1>CarBidApp</LeftAlignedH1></TitleLink>
             </NavbarMenuCol>
             <NavbarMenuCol flex={3}>
-                <FilledRowInput type="text" placeholder="search"/>
+                <Search placeholder="Search for cars" onSearch={onSearch} style={searchStyle} />
             </NavbarMenuCol>
             <NavbarMenuCol flex={1}>
                 <Dropdown overlay={menu} trigger={['click']}>
