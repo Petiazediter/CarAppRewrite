83ad36caaf8fb837ea48e20db889bb4bb513851c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-28 11:02:51 +0200

    Create image slider

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled"
-import {Tag} from "antd"
+import {Tag, Carousel} from "antd"
 
 const CarComponentWrapper = styled('div')`
 
@@ -8,9 +8,20 @@ const CarComponentWrapper = styled('div')`
 export function CarDisplay( {carName, tags, highestBid} : {carName: string, tags: string[], highestBid: number}) {
     return (
     <CarComponentWrapper>
+        <Carousel autoplay>
         <img
         src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
         alt="Primary image of the car"/>
+                   <img
+        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+        alt="Primary image of the car"/>
+                    <img
+        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+        alt="Primary image of the car"/>
+                    <img
+        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+        alt="Primary image of the car"/>
+        </Carousel>
         <h2>{carName}</h2>
         {
         tags.map((item) => 
