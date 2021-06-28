614309a1cd50b932b5937cf61f3ddcf3f99f0167
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-28 11:23:15 +0200

    Create overlay text for image with placeholder text.

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -2,32 +2,52 @@ import styled from "@emotion/styled"
 import {Tag, Carousel} from "antd"
 
 const CarComponentWrapper = styled('div')`
+    width:100%;
+    @media only screen and ( min-width: 800px) {
+        width:400px;
+    }
+`
 
+const BidSpan = styled('span')`
+    position:absolute;
+    top:0;
+    left:0;
+    background:rgba(40,40,40,1);
+    color:white;
+    padding:.2em 1em .2em 1em;
+    border-radius:8px;
+    margin:1.5em 0 0 .5em;
+`
+
+const ImageDiv = styled('div')`
+    position:relative;
 `
 
 export function CarDisplay( {carName, tags, highestBid} : {carName: string, tags: string[], highestBid: number}) {
     return (
     <CarComponentWrapper>
-        <Carousel autoplay>
-        <img
-        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
-        alt="Primary image of the car"/>
-                   <img
-        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
-        alt="Primary image of the car"/>
-                    <img
-        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
-        alt="Primary image of the car"/>
-                    <img
-        src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
-        alt="Primary image of the car"/>
-        </Carousel>
+        <ImageDiv>
+            <Carousel autoplay>
+                <img
+                src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+                alt="Primary image of the car"/>
+                        <img
+                src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+                alt="Primary image of the car"/>
+                            <img
+                src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+                alt="Primary image of the car"/>
+                            <img
+                src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/7a0a3c6148108c9c64425dd85e0181fa3cccb652/photos/3R06APaX.hGgqDLbGi-(edit).jpg?t=162370774158"
+                alt="Primary image of the car"/>
+            </Carousel>
+            <BidSpan>Bid: ${highestBid} | Time remaining: 8:00:00</BidSpan>
+        </ImageDiv>
         <h2>{carName}</h2>
         {
         tags.map((item) => 
                 <Tag>{item}</Tag>
             )
         }
-        <span>${highestBid}</span>
     </CarComponentWrapper>)
 }
