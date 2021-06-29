4ed7ca2e5589f5cdb72af39045c0f285e9de1126
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-29 09:33:19 +0200

    Switch the order between Router and ContentLayout.

--- car-app-frontend/src/App.tsx
+++ car-app-frontend/src/App.tsx
@@ -29,48 +29,48 @@ const TitleLi = styled('li')`
 
 function App() {
   return (
-    <ContentLayout>
-      <BrowserRouter>
-        <Route path="/" exact>
-          <Auctions/>
-        </Route>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Navbar/>
-      </BrowserRouter>
-      <div style={{background: "#EEEEF0", width: '100vw', left:0,bottom:0, position:'absolute'}}>
-          <Row>
-            <Col flex={1}>
-              <h1>CarsBidApp</h1>
-            </Col>
-            <Col flex={1}>
-              <StyledUl>
-                <TitleLi>About us</TitleLi>
-                <li>Sell a car</li>
-                <li>Buy a car</li>
-                <li>Place a bid</li>
-              </StyledUl>
-            </Col>
-            <Col flex={1}>
-              <StyledUl>
-                  <TitleLi>Guarantee</TitleLi>
-                  <li>Terms and agreements</li>
-                  <li>Report a scam</li>
-                  <li>Contact us</li>
+    <BrowserRouter>
+      <ContentLayout>
+          <Route path="/" exact>
+            <Auctions/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Navbar/>
+      </ContentLayout>
+      <div style={{background: "#EEEEF0", width: '100vw', left:0,bottom:0, margin:0}}>
+            <Row>
+              <Col flex={1}>
+                <h1>CarsBidApp</h1>
+              </Col>
+              <Col flex={1}>
+                <StyledUl>
+                  <TitleLi>About us</TitleLi>
+                  <li>Sell a car</li>
+                  <li>Buy a car</li>
+                  <li>Place a bid</li>
                 </StyledUl>
-            </Col>
-            <Col flex={1}>
-              <StyledUl>
-                  <TitleLi>Social Media</TitleLi>
-                  <li>Facebook</li>
-                  <li>Instagram</li>
-                  <li>Twitter</li>
-                </StyledUl>
-            </Col>
-          </Row>
-        </div>
-    </ContentLayout>
+              </Col>
+              <Col flex={1}>
+                <StyledUl>
+                    <TitleLi>Guarantee</TitleLi>
+                    <li>Terms and agreements</li>
+                    <li>Report a scam</li>
+                    <li>Contact us</li>
+                  </StyledUl>
+              </Col>
+              <Col flex={1}>
+                <StyledUl>
+                    <TitleLi>Social Media</TitleLi>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Twitter</li>
+                  </StyledUl>
+              </Col>
+            </Row>
+          </div>
+    </BrowserRouter>
   );
 }
 
