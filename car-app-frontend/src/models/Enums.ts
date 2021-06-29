501a8a38c9ceca1767c2dae9e8350df5a423ac4e
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-29 11:35:01 +0200

    Create Enums.ts for collecting enum objects about models information.

---
+++ car-app-frontend/src/models/Enums.ts
@@ -0,0 +1,28 @@
+export enum Transmission{
+    ALL = 0,
+    MANUAL = 1,
+    AUTOMATIC = 2
+}
+
+export enum CarBody{
+    ALL = 0,
+    COUPE = 1,
+    CONVERTIBLE = 2,
+    HATCHBACK = 3,
+    SEDAN = 4,
+    SUV_CROSSOVER = 5,
+    TRUCK = 6,
+    VAN_MINIVAN = 7,
+    WAGON = 8
+}
+
+export enum DriveTrain {
+    REAR = 0,
+    FRONT = 1,
+    BOTH = 2
+}
+
+export enum UserType{
+    PERSONAL = 0,
+    COMPANY = 1
+}
