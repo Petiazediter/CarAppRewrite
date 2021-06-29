210ef9b5a14c080da47f33862bb280914ea0211e
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-29 10:46:49 +0200

    Update Car model

--- car-app-frontend/src/models/Car.ts
+++ car-app-frontend/src/models/Car.ts
@@ -1,9 +1,30 @@
+import { Seller } from "./Seller";
+
 export interface Car{
     id: number,
-    name: string,
-    tags: string[],
-    bid: number,
+    brand: string,
+    model: string,
+    seller: Seller,
     country: string,
     city: string,
-    imgLinks: string[]
+    vin: string,
+    km: number,
+    body: number,
+    engine: string,
+    drivetrain: number,
+    transmission: number,
+    exterior: string,
+    interior: string,
+    highlightsTitle: string,
+    highLightsItems: string[],
+    equipmentTitle: string,
+    equipmentItems: string[],
+    flaws: string[],
+    serviceHistory: string,
+    extraItems: string[],
+    ownerShipHistory: string,
+    videos: string[],
+    exteriorImages: string[],
+    interiorImages: string[],
+    paperImages: string[]
 }
