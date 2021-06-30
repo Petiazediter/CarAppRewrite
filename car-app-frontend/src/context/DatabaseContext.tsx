bce64637dbae91d8617829a3ea2cb3a8fce38465
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-30 13:01:08 +0200

    Rename context fields.

--- car-app-frontend/src/context/DatabaseContext.tsx
+++ car-app-frontend/src/context/DatabaseContext.tsx
@@ -2,19 +2,19 @@ import React, { ReactElement, useContext} from "react";
 import { Car } from "../models/Car";
 import data from '../placeholder_database.json';
 
-const getUsersTable = (): Car[] => {
+const getCarsTable = (): Car[] => {
     return data.cars;
 }
 
 export function useGetCars(){
     return useContext(GetUsersTableContext)
-};
+}
 
-export const GetUsersTableContext = React.createContext(getUsersTable);
+export const GetUsersTableContext = React.createContext(getCarsTable);
 
 export function DatabaseProvider({children} : {children: ReactElement}) {
     return (
-        <GetUsersTableContext.Provider value={getUsersTable} >
+        <GetUsersTableContext.Provider value={getCarsTable} >
             {children}
         </GetUsersTableContext.Provider>
     )
