32958bb019c6b78ad3a62fa99242894d065961c0
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-30 13:11:52 +0200

    Create file for styles of navbar,

---
+++ car-app-frontend/src/components/shared/navbar-component/Navbar.styled.ts
@@ -0,0 +1,66 @@
+import styled from "@emotion/styled";
+import {Col} from "antd";
+import {Link} from "react-router-dom";
+import {navbarHeight} from "./Navbar";
+
+interface NavbarMenuColProps{
+    isHighlighted?: boolean;
+}
+
+export const NavbarMenuCol = styled(Col)<NavbarMenuColProps>`
+    background: ${(p: NavbarMenuColProps) => p.isHighlighted ? '#fc5c65' : 'none'};
+    color: ${(p: NavbarMenuColProps) => p.isHighlighted ? 'white' : 'black'};
+    font-weight:bolder;
+    text-align:center;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    position:relative;
+    &:hover{
+        background: ${(p:NavbarMenuColProps) => p.isHighlighted ? '#eb3b5a' : 'none'};
+    }
+`
+
+export const StyledNav = styled('nav')`
+    box-shadow: 0 1px 0 0 rgba(100,100,100,.2);
+    height: ${navbarHeight};
+    position:fixed;
+    background:white;
+    width:100%;
+    left:0;
+    top:0;
+    margin:0;
+    padding-top:.5em;
+    @media only screen and ( min-width: 800px) {
+        left:50%;
+        transform:translateX(-50%);
+        width:90.1%;
+    }
+
+      @media only screen and (min-width:1500px){
+        width:70.1%;
+      }
+`
+
+export const NavbarLink = styled(Link)<NavbarMenuColProps>`
+    color:${(p: NavbarMenuColProps) => p.isHighlighted ? '#eee' : 'grey'};
+    &:hover{
+        color:${(p: NavbarMenuColProps) => p.isHighlighted ? 'white' : 'black'};
+    }
+`
+
+export const TitleLink = styled(Link)`
+    position:relative;
+    width:100%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const LeftAlignedH1 = styled('h1')`
+    position:relative;
+    width:100%;
+    padding:0;
+    margin:0;
+    text-align:left;
+`
--- car-app-frontend/src/components/shared/navbar-component/Navbar.tsx
+++ car-app-frontend/src/components/shared/navbar-component/Navbar.tsx
@@ -1,73 +1,11 @@
-import styled from "@emotion/styled"
-import { Row, Col, Menu, AutoComplete, Dropdown, Input } from "antd"
+import { Row, Menu, AutoComplete, Dropdown, Input } from "antd"
 import { Link } from 'react-router-dom'
 import useWindowDimensions from '../../../utils/WindowSize'
 import { MenuOutlined, CarFilled } from '@ant-design/icons'
+import { StyledNav, NavbarLink, NavbarMenuCol, TitleLink, LeftAlignedH1 } from "./Navbar.styled";
 
 const { Search } = Input;
 export const navbarHeight: string = '60px';
-
-interface NavbarMenuColProps{
-    isHighlighted?: boolean;
-}
-
-const NavbarMenuCol = styled(Col)<NavbarMenuColProps>`
-    background: ${(p: NavbarMenuColProps) => p.isHighlighted ? '#fc5c65' : 'none'};
-    color: ${(p: NavbarMenuColProps) => p.isHighlighted ? 'white' : 'black'};
-    font-weight:bolder;
-    text-align:center;
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    position:relative;
-    &:hover{
-        background: ${(p:NavbarMenuColProps) => p.isHighlighted ? '#eb3b5a' : 'none'};
-    }
-`
-
-const StyledNav = styled('nav')`
-    box-shadow: 0 1px 0 0 rgba(100,100,100,.2);
-    height: ${navbarHeight};
-    position:fixed;
-    background:white;
-    width:100%;
-    left:0;
-    top:0;
-    margin:0;
-    padding-top:.5em;
-    @media only screen and ( min-width: 800px) {
-        left:50%;
-        transform:translateX(-50%);
-        width:90.1%;
-    }
-
-      @media only screen and (min-width:1500px){
-        width:70.1%;
-      }
-`
-
-const NavbarLink = styled(Link)<NavbarMenuColProps>`
-    color:${(p: NavbarMenuColProps) => p.isHighlighted ? '#eee' : 'grey'};
-    &:hover{
-        color:${(p: NavbarMenuColProps) => p.isHighlighted ? 'white' : 'black'};
-    }
-`
-
-const TitleLink = styled(Link)`
-    position:relative;
-    width:100%;
-    display:flex;
-    align-items: center;
-    justify-content: center;
-`
-
-const LeftAlignedH1 = styled('h1')`
-    position:relative;
-    width:100%;
-    padding:0;
-    margin:0;
-    text-align:left;
-`
 
 const menu = (
     <Menu>
@@ -110,8 +48,8 @@ const renderTitle = (title:string) => (
         more
       </a>
     </span>
-  );
-  
+);
+
   const renderItem = (title:string) => ({
     value: title,
     label: (
@@ -127,7 +65,8 @@ const renderTitle = (title:string) => (
         </span>
       </div>
     ),
-  });
+});
+
 const options = [
     {
       label: renderTitle('Brands'),
