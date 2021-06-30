10e26f42b805c07a2eaa2040273485cd5bd57f2a
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-06-30 13:05:46 +0200

    Create seperate file for styles to CarDisplay too.

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -1,38 +1,7 @@
-import styled from "@emotion/styled"
 import { Carousel} from "antd"
 import { Link } from "react-router-dom";
 import { Car } from "../../models/Car";
-
-const CarComponentWrapper = styled('div')`
-    position:relative;
-    width:100%;
-    @media only screen and ( min-width: 800px){
-        width:50%;
-        padding:1em;
-    }
-    @media only screen and ( min-width: 1200px){
-        width:33%;
-    }
-`
-
-const BidSpan = styled('span')`
-    position:absolute;
-    top:0;
-    left:0;
-    background:rgba(40,40,40,1);
-    color:white;
-    padding:.2em 1em .2em 1em;
-    border-radius:8px;
-    margin:1.5em 0 0 .5em;
-`
-
-const ImageDiv = styled('div')`
-    position:relative;
-`
-
-const LabelSpan = styled('span')`
-    color:grey;
-`
+import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan} from "./CarDisplay.styled";
 
 export function CarDisplay( {car} : {car: Car}) {
     return (
