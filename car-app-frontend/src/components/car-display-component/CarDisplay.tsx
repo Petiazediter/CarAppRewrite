4e0c4a035c84cc9b58d7be139b785b9c70d6866b
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-01 16:03:19 +0200

    Create placeholder counter for timers.

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -4,6 +4,15 @@ import { Car } from "../../models/Car";
 import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan, CarName,SellerName } from "./CarDisplay.styled";
 
 
+setInterval(() => {
+    const timerElements : NodeListOf<Element> =
+        document.querySelectorAll('.timer');
+    timerElements.forEach((element: Element) => {
+        element.innerHTML = `${Number(element.innerHTML) - 1}`
+    })
+}, 1000)
+
+
 export function CarDisplay( {car} : {car: Car}) {
     return (
         <CarComponentWrapper>
@@ -16,6 +25,7 @@ export function CarDisplay( {car} : {car: Car}) {
                         <LabelSpan>Bid: </LabelSpan>
                         ${car.bids[car.bids.length-1].bid}
                         <LabelSpan> | Time: </LabelSpan>
+                        <span className="timer">10000</span>
                     </BidSpan>
                 </ImageDiv>
                 <Link to={`/car/${car.id}`}>
