7254851d88c15eb098544d4156e4689ec5727e67
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-01 15:10:12 +0200

    Create styled components from seller and carbid title texts

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -1,21 +1,28 @@
 import { Carousel} from "antd"
 import { Link } from "react-router-dom";
 import { Car } from "../../models/Car";
-import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan} from "./CarDisplay.styled";
+import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan, CarName,SellerName } from "./CarDisplay.styled";
+
 
 export function CarDisplay( {car} : {car: Car}) {
     return (
         <CarComponentWrapper>
-            <Link to={`/car/${car.id}`}>
-                <ImageDiv>
-                    <Carousel autoplay>
-                        {car.exteriorImages.map((item,index) => <img key={index} style={{borderRadius: "10px", overflow: "hidden"}} src={item} alt="The car"/>)}
-                    </Carousel>
-                    <BidSpan><LabelSpan>Bid:</LabelSpan> ${car.minBid} | <LabelSpan>Time:</LabelSpan> </BidSpan>
-                </ImageDiv>
-                <h2>{car.title}</h2>
+                <Link to={`/car/${car.id}`}>
+                    <ImageDiv>
+                        <Carousel autoplay>
+                            {car.exteriorImages.map((item,index) => <img key={index} style={{borderRadius: "10px", overflow: "hidden"}} src={item} alt="The car"/>)}
+                        </Carousel>
+                        <BidSpan><LabelSpan>Bid:</LabelSpan> ${car.minBid} | <LabelSpan>Time:</LabelSpan> </BidSpan>
+                    </ImageDiv>
+                </Link>
+                <Link to={`/car/${car.id}`}>
+                    <CarName>{car.title}</CarName>
+                </Link>
+                <Link to={`/user/${car.seller.id}`}>
+                    <SellerName>{car.seller.username}</SellerName>
+                </Link>
                 <p>{car.city},{car.country}</p>
-            </Link>
+
         </CarComponentWrapper>
     )
 }
