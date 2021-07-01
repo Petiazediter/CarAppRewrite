5b8c5ccf06d737911958cfd7842b55814b0fc236
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-01 13:44:19 +0200

    Create methods to filter by endDate.

--- car-app-frontend/src/context/DatabaseContext.tsx
+++ car-app-frontend/src/context/DatabaseContext.tsx
@@ -1,18 +1,59 @@
 import React, {ReactElement, useContext} from "react";
 import { Car } from "../models/Car";
 import data from '../placeholder_database.json';
+import moment from 'moment'
 
-const getCarsTable = (transmission: number = 0,
-                      bodyParts: number = 0): Car[] => {
+export interface CarFilters {
+    bodyStyle: number;
+    transmission: number;
+    country: string;
+    maxPrice: number;
+    minPrice: number;
+    endDate: string;
+}
+
+const getCarsTable = (filters: CarFilters): Car[] => {
     const returnArray: Car[] = [];
-    data.cars.forEach((car: Car) => {
-        if ( car.transmission === transmission || transmission === 0){
-            if ( car.body === bodyParts || bodyParts === 0){
-                returnArray.push(car);
+    data.cars.forEach(car => {
+        if ( filters.bodyStyle === 0 || filters.bodyStyle === car.body){
+            if ( filters.transmission === 0 || filters.transmission === car.transmission){
+                if ( filters.country === 'all' || filters.country === '' || filters.country === car.country){
+                    const date = filters.endDate;
+                    if ( date === '' || isValidDate(date,car.endDate)){
+                        returnArray.push(car);
+                    }
+                }
             }
         }
-    });
+    })
     return returnArray;
+}
+
+function fixURLDate(value: string): string{
+    // Fix the date because january is the 0th month in the URL.
+    let array: string[] = value.split('-');
+    array[1] = (Number(array[1]) + 1).toString();
+    return array.join('-')
+}
+
+function isValidDate(urlDate: string, carDate: string):boolean {
+    urlDate = fixURLDate(urlDate);
+    const moment1 = convertToMoment(urlDate);
+    const moment2 = convertToMoment(carDate);
+    // IF the date from the URL is before the expire date then return true
+    return moment.min([moment1,moment2]) === moment1;
+}
+
+function convertToMoment(dateInString: string): moment.Moment{
+    // Split the date by the separator
+    // Then format the string to Moment type.
+    const array = dateInString.split('-');
+    const formattedDate: moment.MomentInput = {
+        year: Number(array[2]),
+        month: Number(array[1] ) -1,
+        day: Number(array[0])
+    }
+    return moment(formattedDate);
 }
 
 export function useGetCars(){
