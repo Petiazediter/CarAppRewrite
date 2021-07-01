fa34e3b33abfab862d809d590b6063dff48cbca5
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-01 16:49:29 +0200

    Display the difference between the current day and the endDate parameter of the car.

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -2,18 +2,34 @@ import { Carousel, Image} from "antd"
 import { Link } from "react-router-dom";
 import { Car } from "../../models/Car";
 import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan, CarName,SellerName } from "./CarDisplay.styled";
-
+import moment from 'moment'
+import {convertToMoment} from "../../context/DatabaseContext";
 
 setInterval(() => {
     const timerElements : NodeListOf<Element> =
         document.querySelectorAll('.timer');
     timerElements.forEach((element: Element) => {
-        element.innerHTML = `${Number(element.innerHTML) - 1}`
+       const currentText = element.innerHTML;
+       // [days,hours,minutes]
+       const dateArray = currentText.split(':');
+
     })
 }, 1000)
 
 
 export function CarDisplay( {car} : {car: Car}) {
+
+    const getTimeLeft = (endDate: string): string => {
+        const now =  moment();
+        const carDate =  convertToMoment(endDate.replace('/','-'));
+        const differenceBetweenDatesInSeconds = carDate.diff(now,'seconds');
+        const daysLeft = Math.floor(differenceBetweenDatesInSeconds / 86400); // 26686 / 86400 = 0
+        const hoursLeft = Math.floor((differenceBetweenDatesInSeconds % 86400) / 3600); // 26686 / 3600 = 7
+        const minutesLeft = Math.floor(((differenceBetweenDatesInSeconds % 86400) % 3600 ) / 60); // 26686 % 3600 = 24
+        const secondsLeft = Math.floor((((differenceBetweenDatesInSeconds % 86400) % 3600 ) % 60));
+        return `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
+    }
+
     return (
         <CarComponentWrapper>
                 <ImageDiv>
@@ -25,7 +41,7 @@ export function CarDisplay( {car} : {car: Car}) {
                         <LabelSpan>Bid: </LabelSpan>
                         ${car.bids[car.bids.length-1].bid}
                         <LabelSpan> | Time: </LabelSpan>
-                        <span className="timer">10000</span>
+                        <span className="timer">{getTimeLeft(car.endDate)}</span>
                     </BidSpan>
                 </ImageDiv>
                 <Link to={`/car/${car.id}`}>
