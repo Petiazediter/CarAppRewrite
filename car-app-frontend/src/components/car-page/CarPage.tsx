0a47b8c8cd26e3fe90d09c620167e91fcc55f286
Peti <pverebes74@gmail.com>
2021-07-02 13:01:59 +0200

    Set state from prop.

--- car-app-frontend/src/components/car-page/CarPage.tsx
+++ car-app-frontend/src/components/car-page/CarPage.tsx
@@ -5,16 +5,36 @@ interface IUrlProps {
     carId: string | undefined;
 }
 interface IState {
-    carId : string;
+    carId : number;
 }
 
 export class CarPage extends React.Component<RouteComponentProps<IUrlProps>,IState>{
+
+    constructor(props: any){
+        super(props);
+        this.state = {
+            carId: -1
+        }
+    }
+
+    componentDidMount() {
+        const carId: string|undefined = this.props.match.params.carId;
+        if ( carId !== undefined){
+            try{
+                this.setState({
+                    carId: Number(carId)
+                });
+            }catch (e) {
+                console.error('Id is not a number.')
+            }
+        }
+    }
 
     render(){
         return (
             <div>
                 <h1>Hello there!</h1>
-                <h2>Car id: {this.props.match.params.carId}</h2>
+                <h2>Car id: {this.state.carId}</h2>
             </div>
         )
     }
