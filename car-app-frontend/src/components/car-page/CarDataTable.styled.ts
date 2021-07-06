f4cf743c44ba7fb4876414848ec806cb09d594c6
Peti <pverebes74@gmail.com>
2021-07-06 13:51:22 +0200

    add design for table rows.

--- car-app-frontend/src/components/car-page/CarDataTable.styled.ts
+++ car-app-frontend/src/components/car-page/CarDataTable.styled.ts
@@ -4,4 +4,13 @@ export const DataTable = styled('table')`
   width: 100%;
   table-layout: fixed;
   text-align: left;
+  
+  tr{
+    padding:1em;
+    box-sizing: border-box;
+    border-bottom: 1px solid #F6F7F7;
+    td{
+      text-align: right;
+    }
+  }
 `
