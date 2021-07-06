5e8a78d77cf3a5c705a6bc83fd05547b98232e56
Peti <pverebes74@gmail.com>
2021-07-06 07:48:44 +0200

    Update the order of equipments.

--- car-app-frontend/src/components/car-page/CarDataTable.tsx
+++ car-app-frontend/src/components/car-page/CarDataTable.tsx
@@ -6,30 +6,25 @@ export const CarDataTable: FunctionComponent<{car: Car}> = (props) => {
     return (
         <DataTable>
             <tr>
+                <th>Brand</th>
+                <td>{props.car.model} {props.car.brand}</td>
+            </tr>
+            <tr>
                 <th>Engine</th>
                 <td>{props.car.engine}</td>
             </tr>
-
             <tr>
                 <th>Kilometers</th>
                 <td>{props.car.km} km</td>
             </tr>
-
             <tr>
                 <th>Location</th>
                 <td>{props.car.city}, {props.car.country}</td>
             </tr>
-
-            <tr>
-                <th>Brand</th>
-                <td>{props.car.brand}</td>
-            </tr>
-
             <tr>
                 <th>Model</th>
                 <td>{props.car.model}</td>
             </tr>
-
             <tr>
                 <th>VIN</th>
                 <td>{props.car.vin}</td>
