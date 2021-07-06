304ade87fc6836219855a5da2acb512e93a745e7
Peti <pverebes74@gmail.com>
2021-07-06 14:54:10 +0200

    remove unused imports from components.

--- car-app-frontend/src/components/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/car-display-component/CarDisplay.tsx
@@ -1,27 +1,27 @@
-import { Carousel, Image} from "antd"
-import { Link } from "react-router-dom";
-import { Car } from "../../models/Car";
-import { CarComponentWrapper, ImageDiv, BidSpan, LabelSpan, CarName,SellerName } from "./CarDisplay.styled";
+import {Carousel} from "antd"
+import {Link} from "react-router-dom";
+import {Car} from "../../models/Car";
+import {CarComponentWrapper, ImageDiv, BidSpan, LabelSpan, CarName, SellerName} from "./CarDisplay.styled";
 import moment from 'moment'
 import {convertToMoment} from "../../context/DatabaseContext";
 import {FunctionComponent} from "react";
 
 setInterval(() => {
-    const timerElements : NodeListOf<Element> = document.querySelectorAll('.timer');
+    const timerElements: NodeListOf<Element> = document.querySelectorAll('.timer');
     timerElements.forEach((element: Element) => {
         const currentText = element.innerHTML;
         // [days,hours,minutes]
         const dateArray = currentText.split(':');
-        for ( let i = dateArray.length -1; i >= 0; i-- ) {
+        for (let i = dateArray.length - 1; i >= 0; i--) {
             const newValue: number = (Number(dateArray[i]) - 1);
             dateArray[i] = newValue.toString();
-            if ( newValue !== 0) {
+            if (newValue !== 0) {
                 break;
             } else {
-                if ( i === 0 ){
+                if (i === 0) {
                     break;
                 }
-                if ( i === 1){
+                if (i === 1) {
                     dateArray[i] = '23'
                 } else {
                     dateArray[i] = '59'
@@ -33,39 +33,40 @@ setInterval(() => {
 }, 1000)
 
 
-export const CarDisplay: FunctionComponent<{car: Car}> = (props) => {
+export const CarDisplay: FunctionComponent<{ car: Car }> = (props) => {
     const getTimeLeft = (endDate: string): string => {
-        const now =  moment();
-        const carDate =  convertToMoment(endDate.replace('/','-'));
-        const differenceBetweenDatesInSeconds = carDate.diff(now,'seconds');
+        const now = moment();
+        const carDate = convertToMoment(endDate.replace('/', '-'));
+        const differenceBetweenDatesInSeconds = carDate.diff(now, 'seconds');
         const daysLeft = Math.floor(differenceBetweenDatesInSeconds / 86400); // 26686 / 86400 = 0
         const hoursLeft = Math.floor((differenceBetweenDatesInSeconds % 86400) / 3600); // 26686 / 3600 = 7
-        const minutesLeft = Math.floor(((differenceBetweenDatesInSeconds % 86400) % 3600 ) / 60); // 26686 % 3600 = 24
-        const secondsLeft = Math.floor((((differenceBetweenDatesInSeconds % 86400) % 3600 ) % 60));
+        const minutesLeft = Math.floor(((differenceBetweenDatesInSeconds % 86400) % 3600) / 60); // 26686 % 3600 = 24
+        const secondsLeft = Math.floor((((differenceBetweenDatesInSeconds % 86400) % 3600) % 60));
         return `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
     }
 
     return (
         <CarComponentWrapper>
-                <Link to={`/car/${props.car.id}`}>
-                    <ImageDiv>
-                        <Carousel autoplay>
-                            {props.car.exteriorImages.map((item,index) =>
-                                <img key={index} style={{borderRadius: "10px", overflow: "hidden"}} src={item} alt="The car"/>)}
-                        </Carousel>
-                        <BidSpan>
-                            <LabelSpan>Bid: </LabelSpan>
-                            ${props.car.bids[props.car.bids.length-1].bid}
-                            <LabelSpan> | Time: </LabelSpan>
-                            <span className="timer">{getTimeLeft(props.car.endDate)}</span>
-                        </BidSpan>
-                    </ImageDiv>
-                    <CarName>{props.car.title}</CarName>
-                </Link>
-                <Link to={`/user/${props.car.seller.id}`}>
-                    <SellerName>{props.car.seller.username}</SellerName>
-                </Link>
-                <p>{props.car.city},{props.car.country}</p>
+            <Link to={`/car/${props.car.id}`}>
+                <ImageDiv>
+                    <Carousel autoplay>
+                        {props.car.exteriorImages.map((item, index) =>
+                            <img key={index} style={{borderRadius: "10px", overflow: "hidden"}} src={item}
+                                 alt="The car"/>)}
+                    </Carousel>
+                    <BidSpan>
+                        <LabelSpan>Bid: </LabelSpan>
+                        ${props.car.bids[props.car.bids.length - 1].bid}
+                        <LabelSpan> | Time: </LabelSpan>
+                        <span className="timer">{getTimeLeft(props.car.endDate)}</span>
+                    </BidSpan>
+                </ImageDiv>
+                <CarName>{props.car.title}</CarName>
+            </Link>
+            <Link to={`/user/${props.car.seller.id}`}>
+                <SellerName>{props.car.seller.username}</SellerName>
+            </Link>
+            <p>{props.car.city},{props.car.country}</p>
 
         </CarComponentWrapper>
     )
--- car-app-frontend/src/components/car-page/CarPage.tsx
+++ car-app-frontend/src/components/car-page/CarPage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router';
+import {RouteComponentProps} from 'react-router';
 import {Car} from '../../models/Car'
-import {ImageDisplay,NoMarginTitle,
-        NoMarginSubTitle, Category,FlexContainer,
-        CategoryName, CategoriesContainer,HighlightTitle,TableList}
+import {
+    ImageDisplay, NoMarginTitle,
+    NoMarginSubTitle, Category, FlexContainer,
+    CategoryName, CategoriesContainer, HighlightTitle, TableList
+}
     from "./Carpage.styled";
-import {Col, Image, Row, Divider} from "antd";
+import {Image, Divider} from "antd";
 import {PaperClipOutlined, CarFilled, EnterOutlined, VideoCameraFilled} from "@ant-design/icons";
 import {CarDataTable} from "./CarDataTable";
 
 interface IUrlProps {
     carId: string | undefined;
 }
+
 interface IState {
     car: Car | null;
     hook: any;
@@ -25,9 +28,9 @@ enum ImageType {
     VIDEOS
 }
 
-export class CarPage extends React.Component<RouteComponentProps<IUrlProps>,IState>{
+export class CarPage extends React.Component<RouteComponentProps<IUrlProps>, IState> {
 
-    constructor(props: any){
+    constructor(props: any) {
         super(props);
         this.state = {
             car: null,
@@ -37,30 +40,30 @@ export class CarPage extends React.Component<RouteComponentProps<IUrlProps>,ISta
     }
 
     componentDidMount() {
-        const carId: string|undefined = this.props.match.params.carId;
-        if ( carId !== undefined){
-            try{
+        const carId: string | undefined = this.props.match.params.carId;
+        if (carId !== undefined) {
+            try {
                 const car = this.state.hook.getCarById(Number(carId));
                 this.setState({
                     car: car,
                     hook: this.state.hook,
                     imageType: this.state.imageType
                 })
-            }catch (e) {
+            } catch (e) {
                 console.error('Id is not a number.')
             }
         }
     }
 
-    setImageType( imageType: ImageType){
+    setImageType(imageType: ImageType) {
         this.setState({
-            car:this.state.car,
+            car: this.state.car,
             hook: this.state.hook,
             imageType: imageType
         })
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <NoMarginTitle>{this.state.car?.title}</NoMarginTitle>
@@ -70,69 +73,69 @@ export class CarPage extends React.Component<RouteComponentProps<IUrlProps>,ISta
                 <CategoriesContainer>
                     <Category background={"red"} onClick={() => this.setImageType(ImageType.PAPERS)}>
                         <FlexContainer>
-                            <PaperClipOutlined />
+                            <PaperClipOutlined/>
                             <CategoryName className="full-width">Papers</CategoryName>
                         </FlexContainer>
                     </Category>
 
                     <Category background={"orange"} onClick={() => this.setImageType(ImageType.EXTERIOR)}>
                         <FlexContainer>
-                            <CarFilled />
+                            <CarFilled/>
                             <CategoryName className="full-width">Exterior</CategoryName>
                         </FlexContainer>
                     </Category>
 
                     <Category background={"green"} onClick={() => this.setImageType(ImageType.INTERIOR)}>
                         <FlexContainer>
-                            <EnterOutlined />
+                            <EnterOutlined/>
                             <CategoryName className="full-width">Interior</CategoryName>
                         </FlexContainer>
                     </Category>
 
                     <Category background={"blue"} onClick={() => this.setImageType(ImageType.VIDEOS)}>
                         <FlexContainer>
-                            <VideoCameraFilled />
+                            <VideoCameraFilled/>
                             <CategoryName className="full-width">Videos</CategoryName>
                         </FlexContainer>
                     </Category>
                 </CategoriesContainer>
                 < ImageDisplay autoplay={true}>
-                    { this.state.imageType === ImageType.PAPERS ?
-                            this.state.car?.paperImages.map((imgLink:string) =>
-                                <Image alt="Image of car" src={imgLink} />)
+                    {this.state.imageType === ImageType.PAPERS ?
+                        this.state.car?.paperImages.map((imgLink: string) =>
+                            <Image alt="Image of car" src={imgLink}/>)
                         :
                         this.state.imageType === ImageType.EXTERIOR ?
-                            this.state.car?.exteriorImages.map((imgLink:string) =>
-                                <Image  alt="Car exterior" src={imgLink} />)
-                        :
-                        this.state.imageType === ImageType.INTERIOR ?
-                            this.state.car?.interiorImages.map((imgLink:string) =>
-                                <Image  alt="Image of car" src={imgLink} />)
-                        :
-                        <h2>Video format not supported yet</h2>
+                            this.state.car?.exteriorImages.map((imgLink: string) =>
+                                <Image alt="Car exterior" src={imgLink}/>)
+                            :
+                            this.state.imageType === ImageType.INTERIOR ?
+                                this.state.car?.interiorImages.map((imgLink: string) =>
+                                    <Image alt="Image of car" src={imgLink}/>)
+                                :
+                                <h2>Video format not supported yet</h2>
                     }
                 </ImageDisplay>
 
                 {this.state.car != null ?
                     <section>
-                        <CarDataTable car={this.state.car} />
+                        <CarDataTable car={this.state.car}/>
                         <Divider orientation="left">Highlights</Divider>
                         <HighlightTitle>{this.state.car.highlightsTitle}</HighlightTitle>
                         <TableList>
-                            {this.state.car.highLightsItems.map((item:string) =>
+                            {this.state.car.highLightsItems.map((item: string) =>
                                 <li>{item}</li>
                             )}
                         </TableList>
                         <Divider orientation="left">Equipment</Divider>
                         <HighlightTitle>{this.state.car.equipmentTitle}</HighlightTitle>
                         <TableList>
-                            {this.state.car.equipmentItems.map((item:string) =>
+                            {this.state.car.equipmentItems.map((item: string) =>
                                 <li>{item}</li>
                             )}
                         </TableList>
                         <Divider orientation="left">Flaws</Divider>
                         <TableList>
-                            {this.state.car.flaws.map((item:string) =>
+                            {this.state.car.flaws.map((item: string) =>
                                 <li>{item}</li>
                             )}
                         </TableList>
@@ -140,13 +143,13 @@ export class CarPage extends React.Component<RouteComponentProps<IUrlProps>,ISta
                         <HighlightTitle>{this.state.car.serviceHistory}</HighlightTitle>
                         <Divider orientation="left">Extra items</Divider>
                         <TableList>
-                            {this.state.car.extraItems.map((item:string) =>
+                            {this.state.car.extraItems.map((item: string) =>
                                 <li>{item}</li>
                             )}
                         </TableList>
 
                     </section>
-                : <span>NO Data </span>}
+                    : <span>NO Data </span>}
             </div>
         )
     }
