379b8377e0d79a41e81a9375fd74479bf36f6f43
Peti <pverebes74@gmail.com>
2021-07-07 15:28:15 +0200

    Remove everything timer related from CarDisplay component.

--- car-app-frontend/src/components/auctions-page/car-display-component/CarDisplay.tsx
+++ car-app-frontend/src/components/auctions-page/car-display-component/CarDisplay.tsx
@@ -7,63 +7,9 @@ import {convertToMoment} from "../../../context/DatabaseContext";
 import {FunctionComponent} from "react";
 import {TimerText} from "../TimerText";
 
-setInterval(() => {
-    const timerElements: NodeListOf<Element> = document.querySelectorAll('.timer');
-    timerElements.forEach((element: Element) => {
-        const currentText = element.innerHTML;
-        // [days,hours,minutes]
-        const dateArray = currentText.split(':');
-
-        // Start the loop from seconds to days.
-        for ( let index = dateArray.length -1; index >= 0; index--  ){
-            // In this example the index is the last element so we check the seconds at this time.
-            // Get the seconds by the index and decrease it by one.
-            let newValue = Number(dateArray[index]) - 1;
-            if ( newValue > 0){
-                dateArray[index] = newValue.toString();
-                // If the second is greater than 0 that means we want to decrease only the second
-                // And not mocking the minutes , hours or days. So we break the loop.
-                break;
-                // We will not continue the loop to the minutes, so we will not decreasing it.
-            } else {
-                // If the second is reached the zero, that means we want to decrease the minutes too.
-                // So we'll not break the loop, and that means the next loop the minutes will decrease by one
-                // We only had to set the seconds back to 59
-                if ( index >= (dateArray.length - 2)){
-                    // If we checking the seconds or minutes at the moment:
-                    dateArray[index] = '59'
-                    // Set the seconds ( or minutes ) back to 59
-                } else if ( index === 1) {
-                    // If we checking the hours and it's reached zero set it back to 23
-                    dateArray[index] = '23'
-                }
-                // We don't need to check the days.
-            }
-        }
-
-        /*if ( Number(dateArray[0]) > 0){
-            element.innerHTML = `${dateArray[0]} days`
-        } else if ( Number(dateArray[1]) > 0) {
-            element.innerHTML = `${dateArray[1]} hours`
-        }else{ */
-            element.innerHTML = dateArray.join(':')
-        //}
-        console.log(dateArray.join(':'));
-    })
-}, 1000)
 
 
 export const CarDisplay: FunctionComponent<{ car: Car }> = (props) => {
-    const getTimeLeft = (endDate: string): string => {
-        const now = moment();
-        const carDate = convertToMoment(endDate.replace('/', '-'));
-        const differenceBetweenDatesInSeconds = carDate.diff(now, 'seconds');
-        const daysLeft = Math.floor(differenceBetweenDatesInSeconds / 86400); // 26686 / 86400 = 0
-        const hoursLeft = Math.floor((differenceBetweenDatesInSeconds % 86400) / 3600); // 26686 / 3600 = 7
-        const minutesLeft = Math.floor(((differenceBetweenDatesInSeconds % 86400) % 3600) / 60); // 26686 % 3600 = 24
-        const secondsLeft = Math.floor((((differenceBetweenDatesInSeconds % 86400) % 3600) % 60));
-        return `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
-    }
 
     return (
         <CarComponentWrapper>
