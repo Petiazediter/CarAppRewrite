c33d0d7830070a54f73ccb0cee92f8442663ac48
Peti <pverebes74@gmail.com>
2021-07-07 15:29:00 +0200

    Migrate getTimeLeft from CarDisplay method and set the inital state to the return value.

--- car-app-frontend/src/components/auctions-page/TimerText.tsx
+++ car-app-frontend/src/components/auctions-page/TimerText.tsx
@@ -6,8 +6,18 @@ export const TimerText: FunctionComponent<{ fromDate: string }> = (props) => {
 
     const decreaseTimer = (): void => {
         setCurrentDate('Hello world');
+    const getTimeLeft = (endDate: string): string => {
+        const now = moment();
+        const carDate = convertToMoment(endDate.replace('/', '-'));
+        const differenceBetweenDatesInSeconds = carDate.diff(now, 'seconds');
+        const daysLeft = Math.floor(differenceBetweenDatesInSeconds / 86400); // 26686 / 86400 = 0
+        const hoursLeft = Math.floor((differenceBetweenDatesInSeconds % 86400) / 3600); // 26686 / 3600 = 7
+        const minutesLeft = Math.floor(((differenceBetweenDatesInSeconds % 86400) % 3600) / 60); // 26686 % 3600 = 24
+        const secondsLeft = Math.floor((((differenceBetweenDatesInSeconds % 86400) % 3600) % 60));
+        return `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
     }
 
+    const [currentDate, setCurrentDate] = useState(getTimeLeft(props.fromDate))
     const getCurrentDate = (): string => {
         return currentDate;
     }
