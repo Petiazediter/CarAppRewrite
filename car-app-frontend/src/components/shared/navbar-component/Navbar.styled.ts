a739c896a944b3b141143e2c9b337f15f379208d
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-09 13:39:05 +0200

    Replace StyledNav to object styled form

--- car-app-frontend/src/components/shared/navbar-component/Navbar.styled.ts
+++ car-app-frontend/src/components/shared/navbar-component/Navbar.styled.ts
@@ -37,6 +37,7 @@ export const NavbarMenuCol = styled(Col)<NavbarMenuColProps>(props => ({
     }
 }))
 
+/*
 export const StyledNav = styled('nav')`
     z-index:10;
     box-shadow: 0 1px 0 0 rgba(100,100,100,.2);
@@ -58,7 +59,30 @@ export const StyledNav = styled('nav')`
         width:70.1%;
       }
 `
+ */
 
+export const StyledNav = styled.nav({
+    zIndex: 10,
+    boxShadow: '0 1px 0 0 rgba(100,100,100,.2)',
+    height: navbarHeight,
+    position: 'fixed',
+    background: 'white',
+    width: '100%',
+    left: 0,
+    top: 0,
+    margin: 0,
+    paddingTop: '.5em',
+    ['@media only screen and (min-width: 800px)']: {
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: '90.1%'
+    },
+    ['@media only screen and (min-width: 1500px)']: {
+        width: '70.1%'
+    }
+})
+
+/*
 export const NavbarLink = styled(Link)<NavbarMenuColProps>`
     color:${(p: NavbarMenuColProps) => p.highlight ? '#eeeeee' : 'grey'};
     &:hover{
