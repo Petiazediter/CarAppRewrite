075d09e631a6f50d845ef5e40205072ed582dd7d
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-09 13:26:40 +0200

    Replace TableList to object styled form

--- car-app-frontend/src/components/car-page/Carpage.styled.ts
+++ car-app-frontend/src/components/car-page/Carpage.styled.ts
@@ -152,6 +152,7 @@ export const HighlightTitle = styled.h1({
     fontStyle: 'italic'
 })
 
+/*
 export const TableList = styled('ul')`
   list-style: none;
   font-weight: bolder;
@@ -166,3 +167,19 @@ export const TableList = styled('ul')`
     background: #D7DCE7;
   }
 `
+ */
+
+export const TableList = styled.ul({
+    listStyle: 'none',
+    fontWeight: 'bolder',
+    padding: 0,
+    margin: 0,
+    fontFamily: 'sans-serif',
+    ['li']: {
+        padding: '1em',
+        background: '#c8cdd9'
+    },
+    ['li:nth-child(even)']: {
+        background: '#d7dce7'
+    }
+})
