dbbbdc529cbcc28707fa04371d0f8c996ecc94fd
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-12 14:36:56 +0200

    Create login form.

--- car-app-frontend/src/components/sign-in-page/SignInPage.tsx
+++ car-app-frontend/src/components/sign-in-page/SignInPage.tsx
@@ -1,7 +1,50 @@
 import {FunctionComponent} from "react";
+import {Button, Checkbox, Form, Input} from "antd";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import { Link } from 'react-router-dom'
 
 export const SignInPage: FunctionComponent = () => {
-    return (<>
-        hello world
-    </>)
+
+    const onFinish = (values: { username: string,password: string }) => {}
+
+    return (
+        <section>
+            <Form
+                 name="login_form"
+                 className="login-form"
+                 initialValues={{remember: true}}
+                 onFinish={onFinish}>
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {required: true, message: 'Username is required!'}
+                    ]}>
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                           placeholder={"Username"} />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {required: true, message: 'Password is required'}
+                    ]}>
+                    <Input type="password"
+                           placeholder="Password"
+                           prefix={<LockOutlined className="site-form-item-icon" /> } />
+                </Form.Item>
+                <Form.Item
+                    name="remember-me"
+                    valuePropName={"checked"}
+                    noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <Form.Item>
+                    <Button type={"primary"}
+                            htmlType={"submit"}
+                            className={"login-form-button"}>
+                        Log in
+                    </Button>
+                    Or <Link to={"/register"}>register now</Link>
+                </Form.Item>
+            </Form>
+        </section>)
 }
