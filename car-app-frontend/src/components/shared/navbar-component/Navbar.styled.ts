4ba5f4475a201ec2267a15fb1f9bbee00b178199
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-13 19:05:08 +0200

    Replace boolean type of prop to string type of prop.

--- car-app-frontend/src/components/shared/navbar-component/Navbar.styled.ts
+++ car-app-frontend/src/components/shared/navbar-component/Navbar.styled.ts
@@ -1,67 +1,72 @@
-import styled from "@emotion/styled";
-import {Col} from "antd";
-import {Link} from "react-router-dom";
+import styled from '@emotion/styled';
+import { Col } from 'antd';
+import { Link } from 'react-router-dom';
 
+export const IS_HIGHLIGHTED = 'ishighlighted';
 export const navbarHeight: string = '60px';
 
-interface NavbarMenuColProps{
-    highlight?: boolean;
+interface NavbarMenuColProps {
+	highlight?: string;
 }
 
-export const NavbarMenuCol = styled(Col)<NavbarMenuColProps>(props => ({
-    background: `${props.highlight ? '#fc5c65' : 'none'}`,
-    color: props.highlight ? 'white' : 'black',
-    fontWeight: 'bolder',
-    textAlign: 'center',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-    '&:hover': {
-        background: props.highlight ? '#eb3b5a': 'none'
-    }
-}))
+export const NavbarMenuCol = styled(Col)<NavbarMenuColProps>(
+	(props: NavbarMenuColProps) => ({
+		background: `${props.highlight === IS_HIGHLIGHTED ? '#fc5c65' : 'none'}`,
+		color: `${props.highlight === IS_HIGHLIGHTED ? 'white' : 'black'}`,
+		fontWeight: 'bolder',
+		textAlign: 'center',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		position: 'relative',
+		'&:hover': {
+			background: props.highlight === IS_HIGHLIGHTED ? '#eb3b5a' : 'none',
+		},
+	})
+);
 
 export const StyledNav = styled.nav({
-    zIndex: 10,
-    boxShadow: '0 1px 0 0 rgba(100,100,100,.2)',
-    height: navbarHeight,
-    position: 'fixed',
-    background: 'white',
-    width: '100%',
-    left: 0,
-    top: 0,
-    margin: 0,
-    paddingTop: '.5em',
-    '@media only screen and (min-width: 800px)': {
-        left: '50%',
-        transform: 'translateX(-50%)',
-        width: '90.1%'
-    },
-    '@media only screen and (min-width: 1500px)': {
-        width: '70.1%'
-    }
-})
+	zIndex: 10,
+	boxShadow: '0 1px 0 0 rgba(100,100,100,.2)',
+	height: navbarHeight,
+	position: 'fixed',
+	background: 'white',
+	width: '100%',
+	left: 0,
+	top: 0,
+	margin: 0,
+	paddingTop: '.5em',
+	'@media only screen and (min-width: 800px)': {
+		left: '50%',
+		transform: 'translateX(-50%)',
+		width: '90.1%',
+	},
+	'@media only screen and (min-width: 1500px)': {
+		width: '70.1%',
+	},
+});
 
-export const NavbarLink = styled(Link)<NavbarMenuColProps>(props => ({
-    color: props.highlight ? '#eeeeee': 'grey',
-    '&:hover': {
-        color: props.highlight ? 'white' : 'black'
-    }
-}))
+export const NavbarLink = styled(Link)<NavbarMenuColProps>(
+	(props: NavbarMenuColProps) => ({
+		color: props.highlight === IS_HIGHLIGHTED ? '#eeeeee' : 'grey',
+		'&:hover': {
+			color: props.highlight === IS_HIGHLIGHTED ? 'white' : 'black',
+		},
+	})
+);
 
 export const TitleLink = styled(Link)({
-    position: 'relative',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-})
+	position: 'relative',
+	width: '100%',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+});
 
 export const LeftAlignedH1 = styled.h1({
-    position: 'relative',
-    width: '100%',
-    padding: 0,
-    margin: 0,
-    textAlign: 'left'
-})
+	position: 'relative',
+	width: '100%',
+	padding: 0,
+	margin: 0,
+	textAlign: 'left',
+});
