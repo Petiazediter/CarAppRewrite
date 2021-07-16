30d5392ad5a308313db2b84e8abdbc65e5af2e78
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-16 13:01:31 +0200

    Create styled component for container.

---
+++ car-app-frontend/src/components/register-page/RegisterPage.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const RegisterContainerSection = styled.section({
+	width: '800px',
+	margin: 'auto',
+	background: '#F8F7F7',
+});
--- car-app-frontend/src/components/register-page/RegisterPage.tsx
+++ car-app-frontend/src/components/register-page/RegisterPage.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from 'react';
+import { RegisterContainerSection } from './RegisterPage.styled';
 
 const RegisterPage: FunctionComponent = () => {
 	return (
-		<>
+		<RegisterContainerSection>
 			<h1>Hello register world</h1>
-		</>
+		</RegisterContainerSection>
 	);
 };
 
