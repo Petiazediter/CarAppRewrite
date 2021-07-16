7e10d09a65a54880c1d757ff2a5e541a9d916727
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-16 13:12:44 +0200

    Create the skeleton of the register fields.

--- car-app-frontend/src/components/register-page/RegisterPage.styled.ts
+++ car-app-frontend/src/components/register-page/RegisterPage.styled.ts
@@ -4,6 +4,7 @@ export const RegisterContainerSection = styled.section({
 	width: '800px',
 	margin: 'auto',
 	background: '#F8F7F7',
+	padding: '1em',
 	h1: {
 		width: '100%',
 		textAlign: 'center',
--- car-app-frontend/src/components/register-page/RegisterPage.tsx
+++ car-app-frontend/src/components/register-page/RegisterPage.tsx
@@ -1,12 +1,41 @@
 import { FunctionComponent } from 'react';
 import { RegisterContainerSection } from './RegisterPage.styled';
 
+import { Button, Form, Input } from 'antd';
+
 const RegisterPage: FunctionComponent = () => {
 	return (
 		<RegisterContainerSection>
 			<h1>
 				Register to <b>CardBidApp</b>
 			</h1>
+			<Form>
+				<Form.Item>
+					<Input
+						type="text"
+						name="username"
+						placeholder="Input your username here."
+					></Input>
+					<Input
+						type="password"
+						name="password"
+						placeholder="Input your password here."
+					></Input>
+					<Input
+						type="password"
+						name="password_verify"
+						placeholder="Verify your password here."
+					></Input>
+					<Input
+						type="email"
+						name="email"
+						placeholder="Input your email-address here."
+					></Input>
+					<Button type="primary" htmlType="submit">
+						Register
+					</Button>
+				</Form.Item>
+			</Form>
 		</RegisterContainerSection>
 	);
 };
