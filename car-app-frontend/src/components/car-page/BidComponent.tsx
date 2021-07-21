df143e865cddc403e3f54cd8f9e2355ab180fa55
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-07-21 11:58:52 +0200

    Add prop type to BidComponent.

--- car-app-frontend/src/components/car-page/BidComponent.tsx
+++ car-app-frontend/src/components/car-page/BidComponent.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Car } from '../../models/Car';
 
-export default class BidComponent extends React.Component {
+export type BidComponentProps = {
+	car: Car;
+};
+
+export default class BidComponent extends React.Component<BidComponentProps> {
 	render() {
 		return <h1>Hello bid component</h1>;
 	}
