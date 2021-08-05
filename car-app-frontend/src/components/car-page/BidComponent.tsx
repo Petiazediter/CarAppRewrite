2588ffa27bf895f694186cc6c5b0db5881272bee
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-05 10:49:06 +0200

    Resolve state/props errors.

--- car-app-frontend/src/components/car-page/BidComponent.tsx
+++ car-app-frontend/src/components/car-page/BidComponent.tsx
@@ -22,11 +22,23 @@ export default class BidComponent extends React.Component<
 	BidComponentProps,
 	BidComponentState
 > {
+	state = {
+		isDark: '',
+		bidsLenght: 0,
+	};
+
 	constructor(props: BidComponentProps) {
 		super(props);
 		this.setState({
 			isDark: props.isDark ? IS_DARK : '',
-			bidsLenght: props.car.bids.length,
+			bidsLenght: props.car.bids.length - 1,
+		});
+	}
+
+	componentDidMount() {
+		this.setState({
+			isDark: this.props.isDark ? IS_DARK : '',
+			bidsLenght: this.props.car.bids.length - 1,
 		});
 	}
 
@@ -37,10 +49,10 @@ export default class BidComponent extends React.Component<
 				isDark: this.props.isDark ? IS_DARK : '',
 			});
 		}
-		if (prevProps.car.bids.length !== this.state.bidsLenght) {
+		if (prevProps.car.bids.length - 1 !== this.state.bidsLenght) {
 			this.setState({
 				...this.state,
-				bidsLenght: this.props.car.bids.length,
+				bidsLenght: this.props.car.bids.length - 1,
 			});
 		}
 	}
@@ -51,10 +63,10 @@ export default class BidComponent extends React.Component<
 				<Bid isdark={this.state.isDark}>
 					<BidDetails isdark={this.state.isDark}>
 						<span>
-							Highest bid: ${this.props.car.bids[this.state.bidsLenght - 1].bid}
+							Highest bid: ${this.props.car.bids[this.state.bidsLenght].bid}
 						</span>
 						<span>
-							{this.props.car.bids[this.state.bidsLenght - 1].user.username}
+							{this.props.car.bids[this.state.bidsLenght].user.username}
 						</span>
 					</BidDetails>
 					<BidButton>Place a bid</BidButton>
