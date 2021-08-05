6b65841c92207bfcafc2aeee034eb510195e8128
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-05 10:35:52 +0200

    reduce code in BidComponent.tsx

--- car-app-frontend/src/components/car-page/BidComponent.tsx
+++ car-app-frontend/src/components/car-page/BidComponent.tsx
@@ -13,21 +13,48 @@ export type BidComponentProps = {
 	isDark: boolean;
 };
 
-export default class BidComponent extends React.Component<BidComponentProps> {
+export type BidComponentState = {
+	isDark: string;
+	bidsLenght: number;
+};
+
+export default class BidComponent extends React.Component<
+	BidComponentProps,
+	BidComponentState
+> {
+	constructor(props: BidComponentProps) {
+		super(props);
+		this.setState({
+			isDark: props.isDark ? IS_DARK : '',
+			bidsLenght: props.car.bids.length,
+		});
+	}
+
+	componentDidUpdate(prevProps: BidComponentProps) {
+		if ((prevProps.isDark ? IS_DARK : '') !== this.state.isDark) {
+			this.setState({
+				...this.state,
+				isDark: this.props.isDark ? IS_DARK : '',
+			});
+		}
+		if (prevProps.car.bids.length !== this.state.bidsLenght) {
+			this.setState({
+				...this.state,
+				bidsLenght: this.props.car.bids.length,
+			});
+		}
+	}
+
 	render() {
 		return (
 			<BidContainer>
-				<Bid isdark={this.props.isDark ? IS_DARK : ''}>
-					<BidDetails isdark={this.props.isDark ? IS_DARK : ''}>
+				<Bid isdark={this.state.isDark}>
+					<BidDetails isdark={this.state.isDark}>
 						<span>
-							Highest bid: $
-							{this.props.car.bids[this.props.car.bids.length - 1].bid}
+							Highest bid: ${this.props.car.bids[this.state.bidsLenght - 1].bid}
 						</span>
 						<span>
-							{
-								this.props.car.bids[this.props.car.bids.length - 1].user
-									.username
-							}
+							{this.props.car.bids[this.state.bidsLenght - 1].user.username}
 						</span>
 					</BidDetails>
 					<BidButton>Place a bid</BidButton>
