cb62aacc5ddba5629c68d27a110cb5663503550c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-05 10:24:28 +0200

    Seperate BidComponent's styled elements to different file.

---
+++ car-app-frontend/src/components/car-page/BidComponent.styled.ts
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+import { Car } from '../../models/Car';
+import { PrimaryButton } from '../auctions-page/Auctions.styled';
+import { IS_DARK } from '../shared/navbar-component/Navbar.styled';
+
+export type BidComponentProps = {
+	car: Car;
+	isDark: boolean;
+};
+
+export type DarkModeProp = {
+	isdark: string;
+};
+
+export const BidContainer = styled.div({
+	width: '100%',
+	display: 'flex',
+	justifyContent: 'center',
+	alignContent: 'center',
+});
+
+export const Bid = styled.section<DarkModeProp>((props) => ({
+	background: props.isdark === IS_DARK ? '#393E46' : 'white',
+	width: '400px',
+	padding: '1em',
+	borderRadius: '22px',
+	boxShadow:
+		props.isdark === IS_DARK
+			? '0 0 4px 4px rgba(0, 0, 0, .2);'
+			: '0 0 4px 4px rgba(0, 0, 255, .2);',
+}));
+
+export const BidDetails = styled.section<DarkModeProp>((props) => ({
+	width: '100%',
+	position: 'relative',
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignContent: 'center',
+	color: 'black',
+}));
+
+export const BidButton = styled(PrimaryButton)({
+	width: '100%',
+	position: 'relative',
+	marginTop: '5px',
+});
--- car-app-frontend/src/components/car-page/BidComponent.tsx
+++ car-app-frontend/src/components/car-page/BidComponent.tsx
@@ -1,50 +1,11 @@
-import styled from '@emotion/styled';
 import React from 'react';
-import { Car } from '../../models/Car';
-import { PrimaryButton } from '../auctions-page/Auctions.styled';
 import { IS_DARK } from '../shared/navbar-component/Navbar.styled';
-
-export type BidComponentProps = {
-	car: Car;
-	isDark: boolean;
-};
-
-export type DarkModeProp = {
-	isdark: string;
-};
-
-const BidContainer = styled.div({
-	width: '100%',
-	display: 'flex',
-	justifyContent: 'center',
-	alignContent: 'center',
-});
-
-const Bid = styled.section<DarkModeProp>((props) => ({
-	background: props.isdark === IS_DARK ? '#393E46' : 'white',
-	width: '400px',
-	padding: '1em',
-	borderRadius: '22px',
-	boxShadow:
-		props.isdark === IS_DARK
-			? '0 0 4px 4px rgba(0, 0, 0, .2);'
-			: '0 0 4px 4px rgba(0, 0, 255, .2);',
-}));
-
-const BidDetails = styled.section<DarkModeProp>((props) => ({
-	width: '100%',
-	position: 'relative',
-	display: 'flex',
-	justifyContent: 'space-between',
-	alignContent: 'center',
-	color: 'black',
-}));
-
-const BidButton = styled(PrimaryButton)({
-	width: '100%',
-	position: 'relative',
-	marginTop: '5px',
-});
+import {
+	Bid,
+	BidButton,
+	BidContainer,
+	BidDetails,
+} from './BidComponent.styled';
 
 export default class BidComponent extends React.Component<BidComponentProps> {
 	render() {
