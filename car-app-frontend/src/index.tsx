d1e589ecfedd4f794aeb08a8205f894913322012
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-24 11:32:15 +0200

    Create apollo client.

--- car-app-frontend/src/index.tsx
+++ car-app-frontend/src/index.tsx
@@ -4,9 +4,62 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { WebSocketLink } from '@apollo/client/link/ws';
+import {
+	ApolloClient,
+	ApolloProvider,
+	HttpLink,
+	InMemoryCache,
+	split,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+export const AUTH_TOKEN = 'auth-token';
+
+const wsLink = new WebSocketLink({
+	uri: 'localhost:4000/graphql',
+	options: {
+		reconnect: true,
+	},
+});
+
+const httpLink = new HttpLink({
+	uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer {token}` : '',
+		},
+	};
+});
+
+const link = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind === 'OperationDefinition' &&
+			definition.operation === 'subscription'
+		);
+	},
+	wsLink,
+	authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+	link: link,
+	cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
