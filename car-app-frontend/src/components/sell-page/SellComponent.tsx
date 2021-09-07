06b5c30a81a471bc5efe139e79302af555281e87
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-09-07 13:30:53 +0200

    Create the layout of the submit form.2

--- car-app-frontend/src/components/sell-page/SellComponent.tsx
+++ car-app-frontend/src/components/sell-page/SellComponent.tsx
@@ -1,7 +1,130 @@
-import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import { Form, Steps, Input, Select, Button } from 'antd';
+import React, { FunctionComponent, useState } from 'react';
+const { Step } = Steps;
+
+const Title = styled.h1({
+	marginTop: 10,
+	fontFamily: 'Roboto',
+	fontWeight: 900,
+});
+
+enum Transmission {
+	MANUAL = 'Manual',
+	AUTOMATIC = 'Automatic',
+}
+
+export enum DriveTrain {
+	REAR = 'Rear',
+	FRONT = 'Front',
+}
+
+export enum Body {
+	COUPE = 'Coupe',
+	CONVERTIBLE = 'Convertible',
+	HATCHBACK = 'Hatchback',
+	SEDAN = 'Sedan',
+	SUV = 'Suv',
+	TRUCK = 'Truck',
+	VAN = 'Van',
+	WAGON = 'Wagon',
+}
 
 const SellComponent: FunctionComponent = () => {
-	return <div>Hello world!</div>;
+	const [page, setPage] = useState<number>(1);
+
+	return (
+		<section>
+			<Steps size="small" current={page - 1}>
+				<Step title="Basic information" />
+				<Step title="Images" />
+				<Step title="Submit" />
+			</Steps>
+
+			<Title>Let's sell your car!</Title>
+
+			{page === 1 && (
+				<div>
+					<h1>Basic informations about the car</h1>
+					<Form>
+						<Form.Item label="Auction's title" style={{ width: '50%' }}>
+							<Input />
+						</Form.Item>
+						<Form.Item style={{ width: '50%' }} label="Brand">
+							<Input />
+						</Form.Item>
+
+						<Form.Item style={{ width: '50%' }} label="Model">
+							<Input />
+						</Form.Item>
+
+						<Form.Item style={{ width: '50%' }} label="Transmission">
+							<Select>
+								<Select.Option value={Transmission.AUTOMATIC.valueOf()}>
+									{Transmission.AUTOMATIC.valueOf()}
+								</Select.Option>
+								<Select.Option value={Transmission.MANUAL.valueOf()}>
+									{Transmission.MANUAL.valueOf()}
+								</Select.Option>
+							</Select>
+						</Form.Item>
+						<Form.Item style={{ width: '50%' }} label="Drivetrain">
+							<Select>
+								<Select.Option value={DriveTrain.REAR.valueOf()}>
+									{DriveTrain.REAR.valueOf()}
+								</Select.Option>
+								<Select.Option value={DriveTrain.FRONT.valueOf()}>
+									{DriveTrain.FRONT.valueOf()}
+								</Select.Option>
+							</Select>
+						</Form.Item>
+						<Form.Item style={{ width: '50%' }} label="Body type">
+							<Select>
+								<Select.Option value={Body.COUPE.valueOf()}>
+									{Body.COUPE.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.CONVERTIBLE.valueOf()}>
+									{Body.CONVERTIBLE.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.HATCHBACK.valueOf()}>
+									{Body.HATCHBACK.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.SEDAN.valueOf()}>
+									{Body.SEDAN.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.TRUCK.valueOf()}>
+									{Body.TRUCK.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.VAN.valueOf()}>
+									{Body.VAN.valueOf()}
+								</Select.Option>
+								<Select.Option value={Body.WAGON.valueOf()}>
+									{Body.WAGON.valueOf()}
+								</Select.Option>
+							</Select>
+						</Form.Item>
+						<Form.Item>
+							<section
+								style={{
+									display: 'flex',
+									width: '50%',
+									justifyContent: 'flex-end',
+								}}
+							>
+								<Button
+									htmlType="submit"
+									style={{ width: '50%' }}
+									type="primary"
+								>
+									Submit
+								</Button>
+							</section>
+						</Form.Item>
+					</Form>
+				</div>
+			)}
+		</section>
+	);
 };
 
 export default SellComponent;
